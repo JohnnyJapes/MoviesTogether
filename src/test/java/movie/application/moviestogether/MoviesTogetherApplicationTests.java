b1807917e6d4a1360ba1c731f765437d98250749
Luke Hanrahan <lhanrahan2@gmail.com>
2024-07-10 19:49:29 -0400

    Unit test for findByTitle on movierepo

--- src/test/java/movie/application/moviestogether/MoviesTogetherApplicationTests.java
+++ src/test/java/movie/application/moviestogether/MoviesTogetherApplicationTests.java
@@ -1,13 +1,40 @@
 package movie.application.moviestogether;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import movie.application.moviestogether.dao.MovieRepository;
+import movie.application.moviestogether.entity.Movie;
 
 @SpringBootTest
 class MoviesTogetherApplicationTests {
 
+
+	@Autowired
+	private MovieRepository movieRepo;
+
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+    void testFindbyTitle() throws Exception {
+
+		System.out.println("TEST");
+
+        List<Movie> movies = movieRepo.findByTitle("Scream");
+
+		for (int i = 0; i < movies.size(); i++) {
+
+			System.out.println(movies.get(i).getTitle());
+			
+		}
+
+    }
+
+
 
 }
