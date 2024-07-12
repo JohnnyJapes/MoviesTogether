3883bf2567d15b1c5089f8787a13f58fd971ff14
Luke Hanrahan <lhanrahan2@gmail.com>
2024-07-12 02:19:55 -0400

    sort movies list by rank to make adding and changing ranks straightforward

--- src/main/java/movie/application/moviestogether/entity/WatchList.java
+++ src/main/java/movie/application/moviestogether/entity/WatchList.java
@@ -2,13 +2,17 @@ package movie.application.moviestogether.entity;
 
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OrderBy;
+import jakarta.persistence.OrderColumn;
 import jakarta.persistence.Table;
 
 @Entity
@@ -26,7 +30,8 @@ public class WatchList {
     @Column(name = "name")
     private String name;
 
-    @OneToMany
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @OrderBy("rank ASC")
     @JoinColumn(name = "list_id", referencedColumnName = "list_id")
     private List<ListItem> movies;
 
