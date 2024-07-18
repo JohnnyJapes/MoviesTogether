89347aa4f6e5e8691e2a93f7e62e5774f5591281
Luke Hanrahan <lhanrahan2@gmail.com>
2024-07-18 00:20:24 -0400

    alert class + event repo

--- src/main/java/movie/application/moviestogether/dao/EventRepository.java
+++ src/main/java/movie/application/moviestogether/dao/EventRepository.java
@@ -1,5 +1,9 @@
 package movie.application.moviestogether.dao;
 
-public interface EventRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import movie.application.moviestogether.entity.Event;
+
+public interface EventRepository extends JpaRepository<Event,Integer> {
     
 }
---
+++ src/main/java/movie/application/moviestogether/model/Alert.java
@@ -0,0 +1,26 @@
+package movie.application.moviestogether.model;
+
+public class Alert {
+
+    private String message;
+
+
+    public Alert() {
+    }
+
+
+    public Alert(String message) {
+        this.message = message;
+    }
+
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    
+}
