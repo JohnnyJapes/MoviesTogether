f474747eb6cbc5609ae58eabbea9d7b03137b9a2
JohnnyJapes <lhanrahan2@gmail.com>
2024-07-24 19:11:30 -0400

    event page wip

--- src/main/java/movie/application/moviestogether/controller/EventController.java
+++ src/main/java/movie/application/moviestogether/controller/EventController.java
@@ -10,17 +10,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import movie.application.moviestogether.dao.ListItemRepository;
 import movie.application.moviestogether.entity.Event;
+import movie.application.moviestogether.entity.EventJoinUser;
 import movie.application.moviestogether.entity.ListItem;
 import movie.application.moviestogether.entity.Movie;
+import movie.application.moviestogether.entity.User;
 import movie.application.moviestogether.entity.WatchList;
 import movie.application.moviestogether.model.EventBase;
 import movie.application.moviestogether.service.EventService;
+import movie.application.moviestogether.service.UserService;
 import movie.application.moviestogether.service.WatchListService;
 import movie.application.moviestogether.validation.WatchListValidation;
 
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -29,18 +35,21 @@ public class EventController {
 
     private EventService eventService;
     private WatchListService watchListService;
+    private UserService userService;
 
 
 
     @Autowired
-    public EventController(EventService eventService, WatchListService watchListService) {
+
+    public EventController(EventService eventService, WatchListService watchListService, UserService userService) {
         this.eventService = eventService;
         this.watchListService = watchListService;
-
+        this.userService = userService;
     }
 
 
 
+    //create from the watchlist page
     @PostMapping("/create")
     public String createEventForm(@ModelAttribute("event") EventBase data, Model model, Authentication authentication) {
         
@@ -64,9 +73,18 @@ public class EventController {
 
         return "events/eventForm";
     }
+
+    @GetMapping("/list")
+    public String getEventsPage(Model model, Authentication authentication) {
+        String username = authentication.getName();
+		System.out.println("username: " +username);
+		User user = userService.findByUserName(username);
+
+        List<EventJoinUser> events = user.getEvents();
+
+        model.addAttribute("events", events);
+        return new String();
+    }
     
-
-
     
-
 }
